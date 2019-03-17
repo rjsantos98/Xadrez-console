02d2a2ebc094e066a2df360ae3c148c0b6de1640
Raimundo Santos <rjsmatos98@gmail.com>
2019-03-17 06:04:30 -0300

    Jogada especial promocao

--- xadrez-console/XadrezEntities/PartidaDeXadrez.cs
+++ xadrez-console/XadrezEntities/PartidaDeXadrez.cs
@@ -140,6 +140,25 @@ namespace XadrezEntities
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = Tab.Peca(destino);
+
+            // #jogadaespecial promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
+
+
+
             if (EstaEmXeque(Adversario(JogadorAtual)))
             {
                 Xeque = true;
@@ -158,8 +177,7 @@ namespace XadrezEntities
                 MudaJogador();
             }
 
-            Peca p = Tab.Peca(destino);
-
+            
             // #jogadaespecial en passant
 
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
